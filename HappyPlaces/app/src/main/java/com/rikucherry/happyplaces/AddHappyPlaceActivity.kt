bd12212fcf51dfb7c40ca25ff43f9ffab7592e3a
rikucherry1993 <rikucherry0522@gmail.com>
2021-09-13 23:02:36 +0900

    Add read/capture photo functionality

--- HappyPlaces/app/src/main/java/com/rikucherry/happyplaces/AddHappyPlaceActivity.kt
+++ HappyPlaces/app/src/main/java/com/rikucherry/happyplaces/AddHappyPlaceActivity.kt
@@ -1,10 +1,44 @@
 package com.rikucherry.happyplaces
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener{
+    private var calendar = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+    companion object {
+        private const val REQUEST_GALLERY = 1
+        private const val REQUEST_IMAGE_CAPTURE = 2
+        private const val IMAGE_DIRECTORY = "HappyPlacesImages"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
@@ -15,5 +49,157 @@ class AddHappyPlaceActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+        et_date.setOnClickListener(this)
+        tv_add_image.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems = arrayOf("Select photo from Gallery",
+                "Capture photo from camera")
+                pictureDialog.setItems(pictureDialogItems){
+                    dialog, which ->
+                    when(which) {
+                        0 -> choosePhotoFromGallery()
+                        1 -> shootPhotos()
+                    }
+                }.show()
+            }
+        }
+    }
+
+    private fun shootPhotos(){
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            AlertDialog.Builder(this).setTitle("Error!")
+                .setMessage("Your device don't have camera to use")
+                .setPositiveButton("Confirm"){
+                    dialog, _ -> dialog.dismiss()
+                }.show()
+        } else {
+            //check permissions
+            Dexter.withActivity(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()) {
+                        //open default camera
+                            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{
+                                intent -> startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+                            }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRational()
+                }
+            }).onSameThread().check()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            saveImage(imageBitmap)
+            iv_place_image.setImageBitmap(imageBitmap)
+
+        } else if (requestCode == REQUEST_GALLERY && resultCode == RESULT_OK) {
+            try{
+                val imageBitmap = MediaStore.Images.Media.getBitmap(contentResolver, data?.data)
+                saveImage(imageBitmap)
+                iv_place_image.setImageBitmap(imageBitmap)
+            } catch (e: IOException){
+                e.printStackTrace()
+                Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun choosePhotoFromGallery(){
+        Dexter.withActivity(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object: MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).also{
+                        intent -> startActivityForResult(intent, REQUEST_GALLERY)
+                    }
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showRational()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showRational(){
+        AlertDialog.Builder(this).setMessage(
+            "Permissions required"
+        ).setPositiveButton("Go to settings") {
+            _,_ ->
+            try {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+            }
+        }.setNegativeButton("Cancel") {
+            dialog, _ -> dialog.dismiss()
+        }.show()
+    }
+
+    private fun updateDateInView(){
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        et_date.setText(sdf.format(calendar.time).toString())
+    }
+
+    private fun saveImage(bitmap: Bitmap) : Uri {
+        val wrapper = ContextWrapper(applicationContext)
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE) //only accessible by the calling app
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try {
+            val stream : OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return Uri.parse(file.absolutePath)
     }
 }
